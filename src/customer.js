e48fc62953474a1b729af8751f16939888f74152
Kat White <kat.white.a@gmail.com>
2021-06-11 13:40:14 -0600

    Set up beforeEacch and imported files

---
+++ src/customer.js
@@ -0,0 +1,10 @@
+class Customer {
+    constrcutor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.bookings = [];
+        this.expense = 0;
+    };
+};
+
+export default Customer;
--- test/customer-test.js
+++ test/customer-test.js
@@ -1,11 +1,26 @@
 import chai from 'chai';
 const expect = chai.expect;
+import { testDataCustomer } from '../src/data/test-data-customer.js';
+import Customer from '../src/classes/Customer';
 
-// IMPORT FORMATTING
-// import Ingredient from '../src/classes/Ingredient';
 
 describe('Customer', function () {
-    it('should return true', function () {
-        expect(true).to.equal(true);
+
+    let customer;
+
+    beforeEach('Instantiate customer', () => {
+        customer = new Customer(testDataCustomer[0])
+    })
+
+    it('Should be an instance of Customer', () => {
+        expect(customer).to.be.an.instanceof(Customer);
     });
-});
+
+    it('Should have a name', () => {
+        expect(customer.name).to.equal('Leatha Ullrich');
+    });
+
+    it('Should have an id', () => {
+        expect(customer.id).to.equal(1);
+    });
+

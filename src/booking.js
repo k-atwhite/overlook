9a1d25e26b7c80505aa85d3d7172a66f5be749a0
Kat White <kat.white.a@gmail.com>
2021-06-11 18:24:57 -0600

    Write all tests for initial booking properties

--- src/booking.js
+++ src/booking.js
@@ -2,8 +2,8 @@ class Booking {
     constructor(booking) {
         this.id = booking.id
         this.userID = booking.userID
-        this.date = booking.dad
-        this.roomNuber = booking.roomNumber
+        this.date = booking.date
+        this.roomNumber = booking.roomNumber
         this.roomServiceCharges = []
     }
 }
--- test/booking-test.js
+++ test/booking-test.js
@@ -3,7 +3,6 @@ const expect = chai.expect;
 
 
 import { testDataBooking } from './test-data-booking.js';
-import { testDataRoom } from './test-data-room.js';
 import Booking from '../src/booking';
 
 
@@ -20,19 +19,23 @@ describe('Booking', function () {
     });
 
     it('Should be have an id', () => {
-        expect(booking.id).to.equal.(1);
+        expect(booking.id).to.equal("5fwrgu4i7k55hl6t8");
+    });
+
+    it('Should be have a user id', () => {
+        expect(booking.userID).to.equal(1);
     });
 
     it('Should be have a date', () => {
-        expect(booking.id).to.equal.("2020/02/05");
+        expect(booking.date).to.equal("2020/02/05");
     });
 
-    it('Should be have a roomNumber', () => {
-        expect(booking.id).to.equal.(12);
+    it('Should be have a room number', () => {
+        expect(booking.roomNumber).to.equal(12);
     });
 
     it('Should be have an array of room service charges', () => {
-        expect(booking.id).to.deep.equal.([]);
+        expect(booking.roomServiceCharges).to.deep.equal([]);
     });
 
 })

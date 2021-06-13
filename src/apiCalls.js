2cdadb63ac6343634baef862f599483050fd9fe8
Kat White <kat.white.a@gmail.com>
2021-06-12 22:35:10 -0600

    Add fetch calls for customers, rooms, bookings

---
+++ src/apiCalls.js
@@ -0,0 +1,69 @@
+const fetchAllData = () => {
+    const promises = [fetchCustomerData(), fetchRoomData(), fetchBookingData()]
+    return Promise.all(promises)
+        .catch(err => console.log(`There seems to be a problem: ${err.message}`))
+}
+
+const fetchCustomerData = () => {
+    return fetch("http://localhost:3001/api/v1/customers")
+        .then(response => response.json())
+        .catch(err => console.log(`There seems to be a problem: ${err.message}`))
+}
+
+const fetchRoomData = () => {
+    return fetch("http://localhost:3001/api/v1/rooms")
+        .then(response => response.json())
+        .catch(err => console.log(`There seems to be a problem: ${err.message}`))
+}
+
+const fetchBookingData = () => {
+    return fetch("http://localhost:3001/api/v1/bookings	")
+        .then(response => response.json())
+        .catch(err => console.log(`There seems to be a problem: ${err.message}`))
+}
+
+const addBooking = (data, userID) => {
+
+    const initObj = { userID: userID, date: data.date, roomNumber: data.roomNumber }
+    const init = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(initObj)
+    }
+    return fetch("http://localhost:3001/api/v1/bookings", init)
+        .then(response => response.json())
+        .catch(err => console.log(`There seems to be a problem: ${err.message}`))
+}
+
+// message: 'Booking with id <id> successfully posted', newBooking: <Object with trip info just posted> }
+
+
+// const deleteBooking = (data, userID) => {
+
+//     const initObj = { userID: userID, date: data.date, roomNumber: data.roomNumber }
+//     const init = {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(initObj)
+//     }
+//     return fetch("http://localhost:3001/api/v1/bookings", init)
+//         .then(response => response.json())
+//         .catch(err => console.log(`There seems to be a problem: ${err.message}`))
+// }
+
+export {
+    fetchCustomerData,
+    fetchRoomData,
+    fetchBookingData,
+    addBooking,
+    // deleteBooking
+};
+
+
+// Get single customer	http://localhost:3001/api/v1/customers/<id> where<id> will be a number of a customer’s id
+
+// Delete single booking	http://localhost:3001/api/v1/bookings/<id> where<id> will be a number of a booking’s id	DELETE	none	{ message: Booking #<id> has been deleted 
--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,13 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+// import { fetchAllData, postIng } from './apiCalls';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import '../images/background.jpg'
+import Booking from './booking';
+import Customer from './customer';
+import Room from './room'
+import domUpdates from './dom-updates'
+
 

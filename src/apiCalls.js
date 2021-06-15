8929ada6688ed3aefe553082e605bf34e97cb0e5
Kat White <kat.white.a@gmail.com>
2021-06-15 07:50:22 -0600

    Add post function to api calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,5 @@
+import {onLoad} from './scripts'
+
 const fetchAllData = () => {
     const promises = [fetchCustomerData(), fetchRoomData(), fetchBookingData()]
     return Promise.all(promises)
@@ -37,23 +39,22 @@ const addBooking = (data, userID) => {
         .catch(err => console.log(`There seems to be a problem: ${err.message}`))
 }
 
-// message: 'Booking with id <id> successfully posted', newBooking: <Object with trip info just posted> }
-
-
-// const deleteBooking = (data, userID) => {
-
-//     const initObj = { userID: userID, date: data.date, roomNumber: data.roomNumber }
-//     const init = {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json"
-//         },
-//         body: JSON.stringify(initObj)
-//     }
-//     return fetch("http://localhost:3001/api/v1/bookings", init)
-//         .then(response => response.json())
-//         .catch(err => console.log(`There seems to be a problem: ${err.message}`))
-// }
+const addBooking = (user, dateSelected, roomNum) => {
+    return fetch('http://localhost:3001/api/v1/bookings', {
+        method: 'POST',
+        body: JSON.stringify({
+            userID: user.id,
+            date: dateSelected,
+            roomNumber: roomNum,
+        })
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+    .then(checkForError)
+    .then(() => onLoad())
+    .catch(err => console.error("There seems to be a problem": err))
+}
 
 export {
     fetchAllData,
@@ -61,10 +62,4 @@ export {
     fetchRoomData,
     fetchBookingData,
     addBooking,
-    // deleteBooking
 };
-
-
-// Get single customer	http://localhost:3001/api/v1/customers/<id> where<id> will be a number of a customer’s id
-
-// Delete single booking	http://localhost:3001/api/v1/bookings/<id> where<id> will be a number of a booking’s id	DELETE	none	{ message: Booking #<id> has been deleted 
